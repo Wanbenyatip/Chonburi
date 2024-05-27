e2846beb67ff86966d35dccc12a9bb5b470f7e44
Wanbenyatip <158016374+Wanbenyatip@users.noreply.github.com>
2024-05-27 16:14:37 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,156 @@
+var tourpoint = {
+  
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Bang Saen Beach",
+          "type": "Natural",
+          "level": 4.3
+        },
+        "geometry": {
+          "coordinates": [
+            100.90997954233092,
+            13.290448946854013
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Crocodile f Farm",
+          "type": "Special",
+          "level": 4.2
+        },
+        "geometry": {
+          "coordinates": [
+            100.93916560211534,
+            12.95831824985045
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Amata Spring Country Club",
+          "type": "Special",
+          "level": 4.6
+        },
+        "geometry": {
+          "coordinates": [
+            101.01298138544519,
+            13.416250346294774
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Grand Canyon Chonburi",
+          "type": "Natural",
+          "level": 4.1
+        },
+        "geometry": {
+          "coordinates": [
+            100.9800642883597,
+            13.321569151891254
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ฺBang Phra Reservoir",
+          "type": "Natural",
+          "level": 4.3
+        },
+        "geometry": {
+          "coordinates": [
+            100.97696180084523,
+            13.20850595454722
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "type": "Special",
+          "name": "The Institute of Marine Science of Burapha University",
+          "level": 4.5
+        },
+        "geometry": {
+          "coordinates": [
+            100.92480770877847,
+            13.2848181851015
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Art in Paradise",
+          "type": "Special",
+          "level": 4.4
+        },
+        "geometry": {
+          "coordinates": [
+            100.88965343067167,
+            12.948172853129506
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Sanctuary of Truth",
+          "level": 4.6,
+          "type": "Cultural"
+        },
+        "geometry": {
+          "coordinates": [
+            100.88913822125454,
+            12.972787760142324
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Phra Chuthathut Palace",
+          "type": "Cultural",
+          "level": 4.6
+        },
+        "geometry": {
+          "coordinates": [
+            100.81761477116584,
+            13.1495741450413
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat Pa Amphawan",
+          "type": "Special",
+          "level": 4.7
+        },
+        "geometry": {
+          "coordinates": [
+            101.04245237301888,
+            13.290952287850615
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
